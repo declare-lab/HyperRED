c25d885fd336275a37c639c12098c77a3a7f4eac
chiayewken <chiayewken@gmail.com>
2022-05-23 15:05:09 +0800

    Code formatting on modules/token_embedders/pretrained_encoder.py

--- modules/token_embedders/pretrained_encoder.py
+++ modules/token_embedders/pretrained_encoder.py
@@ -2,11 +2,9 @@ import logging
 
 import torch
 import torch.nn as nn
-
-from transformers import AutoModel
-
-from utils.nn_utils import gelu
 from modules.token_embedders.bert_encoder import BertLinear
+from transformers import AutoModel
+from utils.nn_utils import gelu
 
 logger = logging.getLogger(__name__)
 
@@ -15,7 +13,15 @@ class PretrainedEncoder(nn.Module):
     """This class using pre-trained model to encode token,
     then fine-tuning the pre-trained model
     """
-    def __init__(self, pretrained_model_name, trainable=False, output_size=0, activation=gelu, dropout=0.0):
+
+    def __init__(
+        self,
+        pretrained_model_name,
+        trainable=False,
+        output_size=0,
+        activation=gelu,
+        dropout=0.0,
+    ):
         """This function initialize pertrained model
 
         Arguments:
@@ -29,22 +35,30 @@ class PretrainedEncoder(nn.Module):
 
         super().__init__()
         self.pretrained_model = AutoModel.from_pretrained(pretrained_model_name)
-        logger.info("Load pre-trained model {} successfully.".format(pretrained_model_name))
+        logger.info(
+            "Load pre-trained model {} successfully.".format(pretrained_model_name)
+        )
 
         self.output_size = output_size
 
         if trainable:
-            logger.info("Start fine-tuning pre-trained model {}.".format(pretrained_model_name))
+            logger.info(
+                "Start fine-tuning pre-trained model {}.".format(pretrained_model_name)
+            )
         else:
-            logger.info("Keep fixed pre-trained model {}.".format(pretrained_model_name))
+            logger.info(
+                "Keep fixed pre-trained model {}.".format(pretrained_model_name)
+            )
 
         for param in self.pretrained_model.parameters():
             param.requires_grad = trainable
 
         if self.output_size > 0:
-            self.mlp = BertLinear(input_size=self.pretrained_model.config.hidden_size,
-                                  output_size=self.output_size,
-                                  activation=activation)
+            self.mlp = BertLinear(
+                input_size=self.pretrained_model.config.hidden_size,
+                output_size=self.output_size,
+                activation=activation,
+            )
         else:
             self.output_size = self.pretrained_model.config.hidden_size
             self.mlp = lambda x: x
@@ -72,10 +86,14 @@ class PretrainedEncoder(nn.Module):
             token_type_inputs = torch.zeros_like(seq_inputs)
         mask_inputs = (seq_inputs != 0).long()
 
-        outputs = self.pretrained_model(input_ids=seq_inputs,
-                                        token_type_ids=token_type_inputs,
-                                        attention_mask=mask_inputs)
+        outputs = self.pretrained_model(
+            input_ids=seq_inputs,
+            token_type_ids=token_type_inputs,
+            attention_mask=mask_inputs,
+        )
         last_hidden_state = outputs[0]
         pooled_output = outputs[1]
 
-        return self.dropout(self.mlp(last_hidden_state)), self.dropout(self.mlp(pooled_output))
+        return self.dropout(self.mlp(last_hidden_state)), self.dropout(
+            self.mlp(pooled_output)
+        )
